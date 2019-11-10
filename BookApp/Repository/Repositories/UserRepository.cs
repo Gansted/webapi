f54d8591f0c89587c334ae47cc58c2702401a276
Kristian Brink Gansted <kbri@itu.dk>
2019-11-10 19:05:09 +0100

    Remove duplication implementation of GetUserById

--- BookApp/Repository/Repositories/UserRepository.cs
+++ BookApp/Repository/Repositories/UserRepository.cs
@@ -10,34 +10,42 @@ using Models.ExtendedModels;
 using Unity;
 using System.Data.Entity;
 
-namespace Repository.Repositories {
-    public class UserRepository : BaseRepository<User>, IUserRepository {
-        public UserRepository(IUnityContainer container) : base(container) {
-        }
-
-        protected override IDbSet<User> DbSet => MasterDB.Users;
-
-        public IEnumerable<BookExtended> GetMoreUserBooks(Guid userId, Guid bookId) {
-            var user = GetAll().FirstOrDefault(u => u.Id == userId);
-            return user.Books.Where(b => b.Id != bookId).Select(b =>
-            new BookExtended() {
-                Author = b.Author,
-                Content = b.Content,
-                CreatedDatetime = b.CreatedDatetime,
-                Id = b.Id,
-                Title = b.Title,
-                UserId = b.UserId,
-                UserName = b.User.FirstName + " " + b.User.LastName
-            }
-                );
-        }
-
-        public User GetUserById(Guid userId) {
-            return GetAll().FirstOrDefault(u => u.Id == userId);
-        }
-
-        public User GetUserByName(string name) {
-            return GetAll().FirstOrDefault(u => u.LastName == name);
-        }
+namespace Repository.Repositories
+{
+  public class UserRepository : BaseRepository<User>, IUserRepository
+  {
+    public UserRepository(IUnityContainer container) : base(container)
+    {
     }
+
+    protected override IDbSet<User> DbSet => MasterDB.Users;
+
+    public IEnumerable<BookExtended> GetMoreUserBooks(Guid userId, Guid bookId)
+    {
+      //No point in duplicating code for getting the user by ID
+      var user = GetUserById(userId);
+      return user.Books.Where(b => b.Id != bookId).Select(b =>
+      new BookExtended()
+      {
+        Author = b.Author,
+        Content = b.Content,
+        CreatedDatetime = b.CreatedDatetime,
+        Id = b.Id,
+        Title = b.Title,
+        UserId = b.UserId,
+        UserName = b.User.FirstName + " " + b.User.LastName
+      }
+          );
+    }
+
+    public User GetUserById(Guid userId)
+    {
+      return GetAll().FirstOrDefault(u => u.Id == userId);
+    }
+
+    public User GetUserByName(string name)
+    {
+      return GetAll().FirstOrDefault(u => u.LastName == name);
+    }
+  }
 }
